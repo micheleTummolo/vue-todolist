ea50792951d839f1e35f5bc90d79f4f845df041c
micheleTummolo <tummolomichele02@gmail.com>
2022-12-20 17:37:50 +0100

    Done static to-do list

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                    text: "Task 1",
+                    done: false
+                },
+                {
+                    text: "Task 2",
+                    done: false
+                },
+                {
+                    text: "Task 3",
+                    done: false
+                },
+                {
+                    text: "Task 4",
+                    done: false
+                },
+                {
+                    text: "Task 5",
+                    done: false
+                },
+                {
+                    text: "Task 6",
+                    done: false
+                },
+            ]
+        }
+    },
+
+}).mount('#app')
